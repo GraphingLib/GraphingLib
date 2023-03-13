0f8cf5965a0f5764a8abb2a2a29f32f94014d950
Yannick Lapointe <yannick.lapointe.2@ulaval.ca>
2023-03-12 23:17:37 -0400

    Initial commit

---
+++ Graphing.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+from matplotlib.patches import Polygon
+
+
+class GraphingException(Exception):
+    pass
+
+
+class Figure():
+    def __init__(self, size: tuple=(10,7)):
+        self.figure, self.axes = plt.subplots(figsize=size)
+        self.curves = []
+        self.labels = []
+        self.handles = []
+    
+    def add_curve(self, curve: list):
+        self.curves.append(curve)
+        self.labels.append(curve.label)
+        self.handles.append(curve.handle)
+
+    def generate_figure(self, legend=True, test=False):
+        if self.curves:
+            for curve in self.curves:
+                curve.plot_curve(self.axes)
+            if legend:
+                self.generate_legend()
+            if not test:
+                plt.tight_layout()
+                plt.show()
+        else:
+            raise GraphingException('No curves to be plotted!')
+    
+    def generate_legend(self):
+        pass
+
+
+@dataclass
+class Curve():
+    xdata: list | np.ndarray
+    ydata: list | np.ndarray
+    color: str
+    label: str
+    
+    def set_color(self, color: str or list[str]):
+        self.color = color
+    
+    def plot_curve(self, axes: plt.Axes):
+        self.handle, = axes.plot(self.xdata, self.ydata, color=self.color, label=self.label)
+
+
+class Scatter(Curve):
+    def plot_curve(self, axes: plt.Axes):
+        self.handle = axes.scatter(self.xdata, self.ydata, color=self.color, label=self.label)
+
+
+class Dashed(Curve):
+    pass
+
+
+@dataclass
+class Histogram():
+    xdata: list | np.ndarray
+    bins: int
+    color: str
+    label: str
+
+    def plot_curve(self, axes: plt.Axes):
+        axes.hist(self.xdata, bins=self.bins, color=self.color, label=self.label)
+
+def legend_artist(legend, orig_handle, xdescent, ydescent, width, height, fontsize):
+    xy = np.array([[0,0,1,1,2,2,3,3,4,4,0], [0,4,4,2.5,2.5,5,5,1.5,1.5,0,0]]).T
+    xy[:,0] = width * xy[:,0] / 4 + xdescent
+    xy[:,1] = height * xy[:,1] / 5 - ydescent
+    patch = Polygon(xy, fc='silver', ec='k')
+    return patch
---
+++ Graphing_unittest.py
@@ -0,0 +1,94 @@
+import unittest
+from Graphing import Figure, Curve, Scatter, GraphingException
+from numpy import linspace, pi, sin, ndarray
+from matplotlib.axes import Axes
+from matplotlib.pyplot import subplots, close
+
+
+class TestFigure(unittest.TestCase):
+    def setUp(self):
+        x = linspace(0, 3*pi, 200)
+        self.testFigure = Figure()
+        self.testCurve = Curve(x, sin(x), 'k', 'Test Curve')
+
+    def test_curves_is_list(self):
+        self.assertIsInstance(self.testFigure.curves, list)
+
+    def test_labels_is_list(self):
+        self.assertIsInstance(self.testFigure.labels, list)
+
+    def test_handles_is_list(self):
+        self.assertIsInstance(self.testFigure.handles, list)
+
+    def test_curve_is_added(self):
+        self.testFigure.add_curve(self.testCurve)
+        self.assertIs(self.testFigure.curves[0], self.testCurve)
+
+    def test_axes_is_Axes(self):
+        self.assertIsInstance(self.testFigure.axes, Axes)
+
+    def test_all_curves_plotted(self):
+        self.testFigure.add_curve(self.testCurve)
+        self.testFigure.generate_figure(test=True)
+        self.assertEqual(len(self.testFigure.axes.get_lines()), len(self.testFigure.curves))
+
+    def test_raise_exception_if_no_curve_added(self):
+        self.assertRaises(GraphingException, self.testFigure.generate_figure)
+
+
+class TestCurve(unittest.TestCase):
+    def setUp(self):
+        x = linspace(0, 3*pi, 200)
+        self.testCurve = Curve(x, sin(x), 'k', 'Test Curve')
+        _, self.testAxes = subplots()
+        self.testCurve.plot_curve(self.testAxes)
+
+    def test_xdata_is_list_or_ndarray(self):
+        self.assertIsInstance(self.testCurve.xdata, list | ndarray)
+
+    def test_ydata_is_list_or_ndarray(self):
+        self.assertIsInstance(self.testCurve.ydata, list | ndarray)
+
+    def test_color_is_str(self):
+        self.assertIsInstance(self.testCurve.color, str)
+
+    def test_label_is_str(self):
+        self.assertIsInstance(self.testCurve.label, str)
+
+    def test_color_is_changed(self):
+        self.testCurve.set_color('r')
+        self.assertEqual(self.testCurve.color, 'r')
+
+    def test_curve_is_plotted(self):
+        self.assertEqual(len(self.testAxes.get_lines()), 1)
+
+
+class TestScatter(unittest.TestCase):
+    def setUp(self):
+        x = linspace(0, 3*pi, 200)
+        self.testScatter = Scatter(x, sin(x), 'k', 'Test Curve')
+        _, self.testAxes = subplots()
+        self.testScatter.plot_curve(self.testAxes)
+
+    def test_scatter_is_curve(self):
+        self.assertIsInstance(self.testScatter, Curve)
+
+    def test_xdata_is_list_or_ndarray(self):
+        self.assertIsInstance(self.testScatter.xdata, list | ndarray)
+
+    def test_color_is_str(self):
+        self.assertIsInstance(self.testScatter.color, str)
+
+    def test_label_is_str(self):
+        self.assertIsInstance(self.testScatter.label, str)
+
+    def test_color_is_changed(self):
+        self.testScatter.set_color('r')
+        self.assertEqual(self.testScatter.color, 'r')
+
+    def test_curve_is_plotted(self):
+        self.assertEqual(len(self.testAxes.collections), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
