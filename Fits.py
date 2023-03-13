d9c21968522f5348e8ad946da028f4dd73f8bd23
Gustave Coulombe <magikgus@gmail.com>
2023-03-13 18:18:54 -0400

    Added FitFromSine class

--- Fits.py
+++ Fits.py
@@ -1,10 +1,11 @@
 from Graphing import Curve
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 class FitFromPolynomial(Curve):
     """
-    Create a curve fit (continuous Curve) from an existing curve object.
+    Create a curve fit (continuous Curve) from an existing curve object using a polynomial fit.
     """
     
     def __init__(self, curve_to_be_fit: Curve, degree: int, color: str, label: str):
@@ -39,6 +40,41 @@ class FitFromPolynomial(Curve):
         Returns a linear function using the given coefficients.
         """
         return lambda x: sum(coeff * x**exponent for exponent, coeff in enumerate(self.coeffs))
+    
+    def plot_curve(self, axes: plt.Axes):
+        num_of_points = 500
+        xdata = np.linspace(self.curve_to_be_fit.xdata[0], self.curve_to_be_fit.xdata[-1], num_of_points)
+        ydata = self.function(xdata)
+        self.handle, = axes.plot(xdata, ydata, color=self.color, label=self.label)
+
+
+class FitFromSine(Curve):
+    """
+    Create a curve fit (continuous Curve) from an existing curve object using a sinusoidal fit.
+    """
+    
+    def __init__(self, curve_to_be_fit: Curve, color: str, label: str):
+        self.curve_to_be_fit = curve_to_be_fit
+        self.calculate_parameters()
+        self.function = self.get_sine_func_with_params()
+        self.color = color
+        self.label = label
+    
+    def __str__(self) -> str:
+        return f"{self.amplitude:.3f}sin({self.frequency_rad:.3f}x + {self.phase:.3f}) + {self.vertical_shift:.3f}"
+    
+    def calculate_parameters(self):
+        parameters, self.cov_matrix = curve_fit(self.sine_func_template,
+                                                self.curve_to_be_fit.xdata,
+                                                self.curve_to_be_fit.ydata)
+        self.amplitude, self.frequency_rad, self.phase, self.vertical_shift = parameters
+    
+    @staticmethod
+    def sine_func_template(x, a, b, c, d):
+            return a * np.sin(b*x + c) + d
+    
+    def get_sine_func_with_params(self):
+        return lambda x: self.amplitude * np.sin(self.frequency_rad * x + self.phase) + self.vertical_shift
     
     def plot_curve(self, axes: plt.Axes):
         num_of_points = 500
