f4281b9ff2f9ef71207dc0348a44b80857bb58f8
Yannick Lapointe <yannick.lapointe.2@ulaval.ca>
2023-03-24 22:14:21 -0400

    Moved stuff in subdirectory and added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='GraphingLib',
+    version='0.0.2',
+    description='A simpler way to view data in Python',
+    url='https://github.com/yalap13/GraphingLib.git',
+    packages=['graphinglib'],
+    install_requires=['numpy','scipy','matplotlib','pyyaml']
+)
