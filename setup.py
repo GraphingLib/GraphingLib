a5db2296f1fd8b08743500e78e1f9d9cfbc37c94
Yannick Lapointe <yannick.lapointe.2@ulaval.ca>
2023-03-26 14:36:44 -0400

    Added MANIFEST.in

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     description='A simpler way to view data in Python',
     url='https://github.com/yalap13/GraphingLib.git',
     packages=find_packages(),
-    install_requires=['numpy','scipy','matplotlib','pyyaml']
+    install_requires=['numpy','scipy','matplotlib','pyyaml'],
+    include_package_data=True
 )
