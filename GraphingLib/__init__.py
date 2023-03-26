70e711cf5acb114db5580a1c631aa115330945af
Yannick Lapointe <yannick.lapointe.2@ulaval.ca>
2023-03-26 12:46:26 -0400

    Added imports to __init__.py

--- GraphingLib/__init__.py
+++ GraphingLib/__init__.py
@@ -3,3 +3,7 @@
 
 A simpler way to visualise data.
 """
+
+from .Figure import Figure
+from .Graph_elements import Curve, Scatter, Dashed, Histogram, Hlines, Vlines, GraphingException
+from .Fits import FitFromPolynomial, FitFromSine
