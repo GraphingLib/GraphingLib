c315f6f33fa0b4a1013f95da62813ba0eaaf543b
Gustave Coulombe <magikgus@gmail.com>
2023-04-19 22:22:12 -0400

    Added square root fit to init

--- graphinglib/__init__.py
+++ graphinglib/__init__.py
@@ -6,5 +6,11 @@ A simpler way to visualise data.
 
 from .data_plotting_1d import Curve, Dashed, Histogram, Scatter
 from .figure import Figure
-from .fits import FitFromExponential, FitFromGaussian, FitFromPolynomial, FitFromSine
+from .fits import (
+    FitFromExponential,
+    FitFromGaussian,
+    FitFromPolynomial,
+    FitFromSine,
+    FitFromSquareRoot,
+)
 from .graph_elements import GraphingException, Hlines, Point, Text, Vlines
