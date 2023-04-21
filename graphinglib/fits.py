da3d51e5fd861d43eee03de4c5c0c53546b4c18a
Gustave Coulombe <magikgus@gmail.com>
2023-04-20 22:30:33 -0400

    Square root fit now functional

--- graphinglib/fits.py
+++ graphinglib/fits.py
@@ -262,17 +262,27 @@ class FitFromSquareRoot(Curve):
         self.label = label + " : " + str(self)
 
     def __str__(self) -> str:
-        raise NotImplementedError()
+        return f"{self.parameters[0]:.3f} sqrt(x {'+' if self.parameters[1] > 0 else '-'} {abs(self.parameters[1]):.3f}) {'+' if self.parameters[2] > 0 else '-'} {abs(self.parameters[2]):.3f}"
 
-    def calculate_parameters(self):
-        raise NotImplementedError()
+    def calculate_parameters(self) -> None:
+        parameters, self.cov_matrix = curve_fit(
+            self.square_root_func_template,
+            self.curve_to_be_fit.xdata,
+            self.curve_to_be_fit.ydata,
+            p0=self.guesses,
+        )
+        self.parameters = parameters
+        self.standard_deviation = np.sqrt(np.diag(self.cov_matrix))
 
     @staticmethod
-    def square_root_func_template(x, amplitude, mean, standard_deviation):
-        raise NotImplementedError()
+    def square_root_func_template(x, a, b, c):
+        return a * np.sqrt(x + b) + c
 
     def square_root_func_with_params(self):
-        raise NotImplementedError()
+        return (
+            lambda x: self.parameters[0] * np.sqrt(x + self.parameters[1])
+            + self.parameters[2]
+        )
 
     def plot_element(self, axes: plt.Axes):
         num_of_points = 500
