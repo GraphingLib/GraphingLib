701f0bf98ea51f007adfb4c1215449885e2946b7
Yannick Lapointe <yannick.lapointe.2@ulaval.ca>
2023-05-15 16:33:34 -0400

    Added basic heatmap

---
+++ graphinglib/data_plotting_2d.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.typing import ArrayLike
+from matplotlib.colors import Colormap
+
+
+@dataclass
+class Heatmap:
+    """
+    Basic heatmap class.
+    """
+
+    image: ArrayLike
+    color_map: str | Colormap = "default"
+    normalize: bool = "default"
+    alpha_value: float = 1.0
+    aspect_ratio: str | float = "default"
+    origin_position: str = "default"
+
+    def plot_element(self, axes: plt.Axes):
+        axes.imshow(
+            self.image,
+            cmap=self.color_map,
+            norm=self.normalize,
+            alpha=self.alpha_value,
+            aspect=self.aspect_ratio,
+            origin=self.origin_position,
+        )
