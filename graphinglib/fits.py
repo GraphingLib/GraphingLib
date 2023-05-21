fdd93100370a95e70d214ac8880289eb2ca1675f
Gustave Coulombe <magikgus@gmail.com>
2023-05-21 12:47:03 -0400

    All fit string tests fixed

--- graphinglib/fits.py
+++ graphinglib/fits.py
@@ -124,17 +124,21 @@ class FitFromPolynomial(GeneralFit):
             coeff_chunks.append(self.format_coeff(coeff))
             power_chunks.append(self.format_power(power))
         coeff_chunks[0] = coeff_chunks[0].lstrip("+ ")
-        return "".join(
-            [coeff_chunks[i] + power_chunks[i] for i in range(len(coeff_chunks))]
+        return (
+            "$"
+            + "".join(
+                [coeff_chunks[i] + power_chunks[i] for i in range(len(coeff_chunks))]
+            )
+            + "$"
         )
 
     @staticmethod
     def format_coeff(coeff):
-        return "$ - {0}$".format(abs(coeff)) if coeff < 0 else "$ + {0}$".format(coeff)
+        return " - {0}".format(abs(coeff)) if coeff < 0 else " + {0}".format(coeff)
 
     @staticmethod
     def format_power(power):
-        return "$x^{0}$".format(power) if power != 0 else ""
+        return "x^{0}".format(power) if power != 0 else ""
 
     def polynomial_func_with_params(self):
         """
@@ -170,18 +174,16 @@ class FitFromSine(GeneralFit):
         self._res_curves_to_be_plotted = False
 
     def __str__(self) -> str:
-        part1 = f"${self.amplitude:.3f} \sin({self.frequency_rad:.3f}x$"
+        part1 = f"{self.amplitude:.3f} \sin({self.frequency_rad:.3f}x"
         part2 = (
-            f"$ + {self.phase:.3f})"
-            if self.phase >= 0
-            else f"$ - {abs(self.phase):.3f})$"
+            f" + {self.phase:.3f})" if self.phase >= 0 else f" - {abs(self.phase):.3f})"
         )
         part3 = (
-            f"$ + {self.vertical_shift:.3f}$"
+            f" + {self.vertical_shift:.3f}"
             if self.vertical_shift >= 0
-            else f"$ - {abs(self.vertical_shift):.3f}$"
+            else f" - {abs(self.vertical_shift):.3f}"
         )
-        return part1 + part2 + part3
+        return f"${part1 + part2 + part3}$"
 
     def calculate_parameters(self):
         parameters, self.cov_matrix = curve_fit(
@@ -229,13 +231,13 @@ class FitFromExponential(GeneralFit):
         self._res_curves_to_be_plotted = False
 
     def __str__(self) -> str:
-        part1 = f"${self.parameters[0]:.3f} \exp({self.parameters[1]:.3f}x$"
+        part1 = f"{self.parameters[0]:.3f} \exp({self.parameters[1]:.3f}x"
         part2 = (
-            f"$ + {self.parameters[2]:.3f})$"
+            f" + {self.parameters[2]:.3f})"
             if self.parameters[2] >= 0
-            else f"$ - {abs(self.parameters[2]):.3f})$"
+            else f" - {abs(self.parameters[2]):.3f})"
         )
-        return part1 + part2
+        return f"${part1 + part2}$"
 
     def calculate_parameters(self):
         parameters, self.cov_matrix = curve_fit(
