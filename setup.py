8b22d17eb62aafa9c78f68e9822f747c7114149a
Gustave Coulombe <93359305+Oddball777@users.noreply.github.com>
2023-05-25 00:19:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='GraphingLib',
     version='0.0.1',
     description='An object oriented wrapper combining the functionalities of Matplotlib and Scipy',
+    author='Gustave Coulombe <magikgus@gmail.com>, Yannick Lapointe <yannicklapointe77@gmail.com>'
     url='https://github.com/yalap13/GraphingLib.git',
     packages=find_packages(),
     install_requires=['numpy','scipy','matplotlib','pyyaml'],
