016618b3b192ed06333aa10d770cc5f71e32dce3
Gustave Coulombe <93359305+Oddball777@users.noreply.github.com>
2023-05-24 20:47:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='GraphingLib',
-    version='0.0.2',
-    description='A simpler way to view data in Python',
+    version='0.0.1',
+    description='An object oriented wrapper combining the functionalities of Matplotlib and Scipy',
     url='https://github.com/yalap13/GraphingLib.git',
     packages=find_packages(),
     install_requires=['numpy','scipy','matplotlib','pyyaml'],
