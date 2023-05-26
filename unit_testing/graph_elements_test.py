aa277b84c3cb05a7cb53807bb4c7b317a53c2d6b
GUSTAVE COULOMBE <magikgus@gmail.com>
2023-05-26 16:52:03 -0400

    Added tests for curve get_point_at_x and get_points_at_y

--- unit_testing/graph_elements_test.py
+++ unit_testing/graph_elements_test.py
@@ -28,6 +28,39 @@ class TestCurve(unittest.TestCase):
     def test_label_is_str(self):
         self.assertIsInstance(self.testCurve.label, str)
 
+    def test_from_function(self):
+        self.assertIsInstance(
+            Curve.from_function(
+                lambda x: x**2,
+                0,
+                1,
+                "Test",
+                color="k",
+                line_width=1,
+                line_style="-",
+                number_of_points=400,
+            ),
+            Curve,
+        )
+
+    def test_add_errorbars(self):
+        self.testCurve.add_errorbars(0.1, 0.1)
+        self.testCurve.add_errorbars(0.1, [0.2, 0.3] * 100)
+        self.testCurve.add_errorbars([0.2, 0.3] * 100, 0.1)
+        self.testCurve.add_errorbars(0.3, None)
+        self.testCurve.add_errorbars(None, 0.3)
+
+    def test_get_point_at_x(self):
+        point = self.testCurve.get_point_at_x(0.5)
+        self.assertEqual(point.x, 0.5)
+        self.assertAlmostEqual(point.y, sin(0.5), places=3)
+
+    def test_get_points_at_y(self):
+        points = self.testCurve.get_points_at_y(0)
+        for i, point in enumerate(points):
+            self.assertEqual(point.y, 0)
+            self.assertAlmostEqual(point.x, i * pi, places=3)
+
     def test_curve_is_plotted(self):
         x = linspace(0, 3 * pi, 200)
         self.testCurve = Curve(
