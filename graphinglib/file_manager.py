160dacb39482433f5160853743306833a9fe19c4
Gustave Coulombe <magikgus@gmail.com>
2023-10-03 23:49:27 -0400

    Added FileSaver class

--- graphinglib/file_manager.py
+++ graphinglib/file_manager.py
@@ -19,3 +19,21 @@ class FileLoader:
 
     def _resource_path(self) -> str:
         return f"{path.dirname(__file__)}/default_styles/{self._file_name}.yml"
+
+
+class FileSaver:
+    """
+    This class implements the file saver for the user styles files.
+    """
+
+    def __init__(self, file_name: str, style_prefs: dict) -> None:
+        self._file_name = file_name
+        self._style_prefs = style_prefs
+        self._file_name = self._resource_path()
+
+    def save(self) -> None:
+        with open(self._file_name, "w") as file:
+            yaml.dump(self._style_prefs, file)
+
+    def _resource_path(self) -> str:
+        return f"{path.dirname(__file__)}/default_styles/{self._file_name}.yml"
