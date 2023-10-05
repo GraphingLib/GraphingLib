6fc3be768f00b5063ae22eb02d616bb7047de842
Gustave Coulombe <magikgus@gmail.com>
2023-10-05 11:36:39 -0400

    Added units tests for file manager classes

--- unit_testing/file_manager_test.py
+++ unit_testing/file_manager_test.py
@@ -1,10 +1,30 @@
 import unittest
 
-from graphinglib.file_manager import FileLoader
+from graphinglib.file_manager import FileDeleter, FileLoader, FileSaver
 
 
 class TestFileLoader(unittest.TestCase):
-    def test_resource_path(self):
+    def test_path(self):
         filename = "a_certain_file"
         loader = FileLoader(filename)
-        self.assertEqual(loader._file_name[-(len(filename) + 5) :], f"/{filename}.yml")
+
+        expected_path = f"{loader._config_dir}/{filename}.yml"
+        self.assertEqual(loader._file_location_customs, expected_path)
+
+
+class TestFileSaver(unittest.TestCase):
+    def test_path(self):
+        filename = "a_certain_file"
+        saver = FileSaver(filename, {"color": "red"})
+
+        expected_path = f"{saver._config_dir}/{filename}.yml"
+        self.assertEqual(saver._save_location, expected_path)
+
+
+class TestFileDeleter(unittest.TestCase):
+    def test_path(self):
+        filename = "a_certain_file"
+        deleter = FileDeleter(filename)
+
+        expected_path = f"{deleter._config_dir}/{filename}.yml"
+        self.assertEqual(deleter._file_location, expected_path)
