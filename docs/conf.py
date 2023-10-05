bcf9f1a2e484d0bb17e4bfdb826a880a9738369e
Yannick Lapointe <yannick.lapointe.2@ulaval.ca>
2023-10-04 20:02:06 -0400

    Update favicon

--- docs/conf.py
+++ docs/conf.py
@@ -36,9 +36,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
+html_favicon = "../images/GraphingLib-favicon.ico"
 html_logo = "../images/GraphingLib-Logo-Bolder.svg"
 html_static_path = ["_static"]
-html_favicon = "../images/GraphingLib-favicon.ico"
 
 # -- Extension options -------------------------------------------------------
 
