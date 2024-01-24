c3bc648ed9da9b25410150df166e663c765cc99f
Yannick Lapointe <74150321+yalap13@users.noreply.github.com>
2024-01-24 17:21:30 -0500

    Bypass LookupError
    Still print error message for debugging purposes

--- graphinglib/_version.py
+++ graphinglib/_version.py
@@ -1,5 +1,9 @@
 from setuptools_scm import get_version
 
-version = get_version("..", relative_to=__file__)
+try:
+    version = get_version("..", relative_to=__file__)
+except LookupError as e:
+    print(e)
+    version = "0.0.0.unknown"
 
 __version__ = version
