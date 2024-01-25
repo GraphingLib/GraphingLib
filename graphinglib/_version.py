88d3312bcc953f9c752fc6d6188fd5764a3a93be
yalap13 <yannicklapointe77@gmail.com>
2024-01-25 13:13:45 -0500

    Removed version LookupError print

--- graphinglib/_version.py
+++ graphinglib/_version.py
@@ -3,7 +3,6 @@ from setuptools_scm import get_version
 try:
     version = get_version("..", relative_to=__file__)
 except LookupError as e:
-    print(e)
     version = "0.0.0.unknown"
 
 __version__ = version
