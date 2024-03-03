65ac9498c202d89494a4114fdd054b052d16982b
yalap13 <yannicklapointe77@gmail.com>
2024-03-03 13:25:44 -0500

    Added example_thumbs to static paths

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ["_static"]
+html_static_path = ["_static", "example_thumbs"]
 html_css_files = ["graphinglib.css"]
 html_theme_options = {
     "github_url": "https://github.com/GraphingLib/GraphingLib",
