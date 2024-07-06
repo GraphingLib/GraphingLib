1aad6aa13ecbda0a39df43927b71139f51fbc55c
yalap13 <yannicklapointe77@gmail.com>
2024-07-05 22:44:54 -0400

    Bump version from 1.4.0 to 1.5.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='GraphingLib',
-    version='1.4.0',
-    description='An object oriented wrapper combining the functionalities of Matplotlib and Scipy',
-    author='Gustave Coulombe <magikgus@gmail.com>, Yannick Lapointe <yannicklapointe77@gmail.com>'
-    url='https://github.com/yalap13/GraphingLib.git',
+    name="GraphingLib",
+    version="1.5.0",
+    description="An object oriented wrapper combining the functionalities of Matplotlib and Scipy",
+    author="Gustave Coulombe <magikgus@gmail.com>, Yannick Lapointe <yannicklapointe77@gmail.com>",
+    url="https://github.com/yalap13/GraphingLib.git",
     packages=find_packages(),
-    install_requires=['numpy','scipy','matplotlib','pyyaml'],
+    install_requires=["numpy", "scipy", "matplotlib", "pyyaml"],
     include_package_data=True,
     setup_requires=["setuptools_scm"],
     use_scm_version={
