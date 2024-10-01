17e60dcceb3a26918b4c717ea21d0db3f581032f
Yannick Lapointe <yannicklapointe77@gmail.com>
2024-10-01 11:34:09 -0400

    Canonical url from rtd environment variables

--- docs/conf.py
+++ docs/conf.py
@@ -83,6 +83,10 @@ html_sidebars = {
     "examples/index": [],
     "contributing/index": [],
 }
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
 
 # -- Extension options -------------------------------------------------------
 
@@ -112,4 +116,6 @@ plot_pre_code = "import graphinglib as gl\nimport numpy as np"
 
 opg_site_url = "https://www.graphinglib.org/"
 ogp_image = "https://raw.githubusercontent.com/GraphingLib/GraphingLib/main/images/opengraph_GL.PNG"
-opg_description = "GraphingLib : A Python library for creating publication-quality figures with ease."
+opg_description = (
+    "GraphingLib : A Python library for creating publication-quality figures with ease."
+)
