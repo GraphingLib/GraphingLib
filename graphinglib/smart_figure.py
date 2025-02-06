6a2ac4fc69158c8896ad86648ddf9b658055e16f
Mathieu Marquis <mamar828@ulaval.ca>
2025-02-05 22:59:03 -0500

    multiple elements in same subplot, SmartFigure.__len__

--- graphinglib/smart_figure.py
+++ graphinglib/smart_figure.py
@@ -48,6 +48,9 @@ class SmartFigure:
         self._subfigs = None
         self._reference_label_i = None
 
+    def __len__(self) -> int:
+        return len(self._elements)
+
     def show(
         self
     ) -> None:
@@ -60,28 +63,38 @@ class SmartFigure:
     def _prepare_figure(
         self,
     ) -> None:
+        if len(self) != self._num_rows*self._num_cols:
+            raise GraphingException("Number of elements does not match the grid size")
+        
         gridspec = self._figure.add_gridspec(self._num_rows, self._num_cols)
         self._subfigs = [[None]*self._num_cols for _ in range(self._num_rows)]
 
         # Plottable and subfigure plotting
         for i, element in enumerate(self._elements):
             row_i, col_i = divmod(i, self._num_cols)
-            if isinstance(element, Plottable):
+
+            if isinstance(element, (Plottable, list)):
+                current_elements = element if isinstance(element, list) else [element]
                 subfig = self._figure.add_subfigure(gridspec[row_i, col_i])
-                ax = subfig.add_subplot()       # creates the Axes object used by Plottable objects
-                element._plot_element(
-                    ax,
-                    2,
-                )
-                self._subfigs[row_i][col_i] = subfig
-                if self._reference_labels:
+                ax = subfig.add_subplot()
+                for current_element in current_elements:
+                    if isinstance(current_element, Plottable):
+                        current_element._plot_element(
+                            ax,
+                            2,
+                        )
+                    else:
+                        raise GraphingException(f"Unsupported element type: {type(current_element).__name__}")
+                if self._reference_labels and len(self) > 1:
                     ax.text(
                         0,
                         1,
                         ascii_lowercase[self._reference_label_i] + ")",
-                        transform=ax.transAxes + self._get_translation(),
+                        transform=ax.transAxes + self._get_reflabel_translation(),
                     )
                     self._reference_label_i += 1
+                self._subfigs[row_i][col_i] = subfig
+
             elif isinstance(element, SmartFigure):
                 subfig = self._figure.add_subfigure(gridspec[row_i, col_i])
                 element._figure = subfig        # associates the current subfigure with the nested SmartFigure
@@ -89,20 +102,22 @@ class SmartFigure:
                 element._prepare_figure()
                 self._reference_label_i = element._reference_label_i
                 self._subfigs[row_i][col_i] = subfig
-            elif element is None:
+
+            elif isinstance(element, list):
                 pass
-            else:
+            elif element is not None:
                 raise GraphingException(f"Unsupported element type: {type(element).__name__}")
         
         # Axes labels
         if len(self._elements) == 1:
+            ax = self._figure.get_axes()[0]
             ax.set_xlabel(self._x_label)
             ax.set_ylabel(self._y_label)
         else:
             self._figure.supxlabel(self._x_label, fontsize=plt.rcParams["font.size"])
             self._figure.supylabel(self._y_label, fontsize=plt.rcParams["font.size"])
 
-    def _get_translation(
+    def _get_reflabel_translation(
             self,
     ) -> ScaledTranslation:
         if self._reflabel_loc == "outside":
@@ -136,14 +151,13 @@ sf_2 = SmartFigure(num_rows=1, num_cols=1, elements=[gl.Scatter([0, 1], [5, 10],
 
 two_by_two = SmartFigure(num_rows=2, num_cols=2, elements=[rc() for _ in range(4)])
 
-orange_curve = gl.Curve([0, 1], [0, 1], label="first curve", color="orange")
+orange_curve = gl.Curve([0, 2], [0, 1], label="first curve", color="orange")
 green_curve = gl.Curve([0, 1, 2], [2, 1, 2], label="second curve", color="green")
 elements = [
     two_by_two,
     None,
-    green_curve,
+    [green_curve, orange_curve],
     sf_1
-
 ]
 sf = SmartFigure(num_rows=2, num_cols=2, elements=elements, x_label="main mama x", y_label="main mama y")
 sf.show()
